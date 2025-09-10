c577542d1feaa65e6ffe943651535d78f74540f0
sbx_user1051 <sbx_user1051@169.254.82.3>
2025-09-10 20:55:04 +0000

    Wed Sep 10 20:55:04 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,49 @@
 package io.accelerate.solutions.CHK;
 
-import io.accelerate.runner.SolutionNotImplementedException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CheckoutSolution {
+
+    private static final Map<Character, Integer> PRICE_MAP = new HashMap<>() {{
+        put('A', 50);
+        put('B', 30);
+        put('C', 20);
+        put('D', 15);
+    }};
+
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        if (skus == null) {
+            return -1;
+        }
+
+        Map<Character, Integer> itemCounts = new HashMap<>();
+
+        for (char sku : skus.toCharArray()) {
+            if(!PRICE_MAP.containsKey(sku)) {
+                return -1;
+            }
+            itemCounts.put(sku, itemCounts.getOrDefault(sku, 0) + 1);
+        }
+
+        int totalPrice = 0;
+        
+        for (Map.Entry<Character, Integer> entry : itemCounts.entrySet()) {
+            char sku = entry.getKey();
+            int count = entry.getValue();
+            int price = PRICE_MAP.get(sku);
+
+            if (sku == 'A') {
+                totalPrice += (count / 3) * 130 + (count % 3) * price;
+            } else if (sku == 'B') {
+                totalPrice += (count / 2) * 45 + (count % 2) * price;
+            } else {
+                totalPrice += count * price;
+            }
+        }
+
+        return totalPrice;
+
     }
 }
+
